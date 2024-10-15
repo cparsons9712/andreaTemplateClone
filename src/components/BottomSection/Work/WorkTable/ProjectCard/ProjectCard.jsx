626b3ac757a69ac70fffa7017eb28cd3cf1b830c
christine parsons <clong9712@gmail.com>
2024-10-15 12:46:54 -0400

    format work details on hover

--- src/components/BottomSection/Work/WorkTable/ProjectCard/ProjectCard.jsx
+++ src/components/BottomSection/Work/WorkTable/ProjectCard/ProjectCard.jsx
@@ -1,10 +1,24 @@
 import './projectCard.css'
 
-export const ProjectCard = ({name, image}) => {
+export const ProjectCard = ({name, image, catagory}) => {
+    const categories = Array.isArray(catagory) ? catagory : [];
+
 
     return (
         <div className="projectCard">
             <img src={image} alt={name} className='workImg'/>
+            <div className='projectInfo'>
+                <div className="headingWrapper">
+                    <div className='projectTitle'>{name}</div>
+                    <div className='projectYear'>2024</div>
+                </div>
+                <div className='projectSpacer'></div>
+                <div className="catagories">
+                    {categories.map((element, index) => (
+                        <div key={index}>{element} </div>
+                    ))}
+                </div>
+            </div>
 
         </div>
 )}
--- src/components/BottomSection/Work/WorkTable/WorkTable.jsx
+++ src/components/BottomSection/Work/WorkTable/WorkTable.jsx
@@ -1,5 +1,6 @@
 import './workTable.css'
 import { ProjectCard } from './ProjectCard/ProjectCard'
+import { SortDescending } from '@phosphor-icons/react'
 
 
 
@@ -7,10 +8,13 @@ export const WorkTable = () => {
 
     return (
         <div className="workTable">
-            <ProjectCard name="Moon" image="https://framerusercontent.com/images/4VKqyXX1fcugFHuEe4lGBSmgOa4.jpg?lossless=1"/>
-            <ProjectCard name="Shining" image="https://framerusercontent.com/images/trc0KLRa4LnYkFJO9d8V2nIHTw.jpg?scale-down-to=2048&lossless=1"/>
-            <ProjectCard name="Wisdom" image="https://framerusercontent.com/images/FKkmLeLxvkoUfZIAUBSPP1Gwz58.jpg?lossless=1"/>
-            <ProjectCard name="Miracle" image="https://framerusercontent.com/images/FRWuper0hmhPJ2lEoJJfq377v50.jpg?lossless=1"/>
+            <ProjectCard name="Moon" image="https://framerusercontent.com/images/4VKqyXX1fcugFHuEe4lGBSmgOa4.jpg?lossless=1" catagory={["Product Design,", "Brand Design"]}/>
+
+            <ProjectCard name="Shining" image="https://framerusercontent.com/images/trc0KLRa4LnYkFJO9d8V2nIHTw.jpg?scale-down-to=2048&lossless=1" catagory={["Product Design,", "Brand Design"]}/>
+
+            <ProjectCard name="Wisdom" image="https://framerusercontent.com/images/FKkmLeLxvkoUfZIAUBSPP1Gwz58.jpg?lossless=1" catagory={["Product Design,", "Brand Design"]}/>
+
+            <ProjectCard name="Miracle" image="https://framerusercontent.com/images/FRWuper0hmhPJ2lEoJJfq377v50.jpg?lossless=1" catagory={["Product Design,", "Brand Design"]}/>
 
 
         </div>
