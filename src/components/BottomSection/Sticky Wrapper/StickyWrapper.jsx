be82816449c0c49d1aa77beb2eb6940d57507e0c
christine parsons <clong9712@gmail.com>
2024-10-17 11:24:09 -0400

    create footer

--- src/components/BottomSection/Sticky Wrapper/StickyWrapper.jsx
+++ src/components/BottomSection/Sticky Wrapper/StickyWrapper.jsx
@@ -1,10 +1,22 @@
 import './stickyWrapper.css'
+import { ArrowUpRight } from '@phosphor-icons/react'
 
 export const StickyWrapper = () => {
 
     return (
         <div className="stickyWrapper">
+            <div className='footer'>
+                <div className='contactCTA'>
+                    <div className='footerCTA'>GET IN TOUCH</div>
+                    <ArrowUpRight size={120} weight="bold" className="dynamicIcon"/>
+                </div>
 
+                <div className="footerLinks">
+                    <div className='link socials'>Twitter</div>
+                    <div className='link socials'>Behance</div>
+                    <div className='link socials'>Dribble</div>
+                </div>
+            </div>
 
         </div>
 )}
